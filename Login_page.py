37d07a7aecba5baed1bf7a12f1977e3ccfe847a2
GurjodhSandhu <114323787+GurjodhSandhu@users.noreply.github.com>
2024-02-28 23:37:59 -0800

    Update Login_page.py

--- Login_page.py
+++ Login_page.py
@@ -2,7 +2,6 @@ import streamlit_authenticator as stauth
 import streamlit as st
 import yaml
 from yaml.loader import SafeLoader
-
 with open('config.yaml') as file: #opening data file with user information
     config = yaml.load(file, Loader=SafeLoader)
 
@@ -23,22 +22,30 @@ elif st.session_state["authentication_status"] is False: # if incorrect password
 elif st.session_state["authentication_status"] is None: #if fields login fields are empty
     st.warning('Please enter your username and password')
 
+username = st.session_state["username"] #intilizes value of username to the current user's name
+
+if st.session_state["authentication_status"]:
+    # creating a 'points' category if currently empty
+    if 'points' not in config['credentials']['usernames'][username]:
+        config['credentials']['usernames'][username]['points'] = 1 #set users point balance to 1
+
 if st.session_state["authentication_status"]: #if the user is authenticated currently
     #do stuff here
     st.write(f'Welcome *{st.session_state["name"]}* to comp 370 project')
     st.title("Temperory header")
-    st.write('your username is: ' + st.session_state["username"])
-    st.write('your email is: '+config['credentials']['usernames'][st.session_state["username"]]['email'])
-    st.write('you currently have: '+str(config['credentials']['usernames'][st.session_state["username"]]['points']) + ' points')
-
+    st.write('your username is: ' + username)
+    st.write('your email is: '+config['credentials']['usernames'][username]['email'])
+    st.write('you currently have: '+str(config['credentials']['usernames'][username]['points']) + ' points')
 
 else: #registration for the website
     try:
         email_of_registered_user, username_of_registered_user, name_of_registered_user = authenticator.register_user(preauthorization=False)
         if email_of_registered_user:
             st.success('User registered successfully')
+
     except Exception as e:
         st.error(e)
+
 
 with open('config.yaml', 'w') as file:
     yaml.dump(config, file, default_flow_style=False)
