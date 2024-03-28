a321023c855a5faeb239633be0dba4b7344f8265
GurjodhSandhu <114323787+GurjodhSandhu@users.noreply.github.com>
2024-03-27 19:31:29 -0700

    Update Login_page.py

--- Login_page.py
+++ Login_page.py
@@ -6,20 +6,7 @@ from yaml.loader import SafeLoader
 
 # Sets page in widemode,specifically allowing header to span across page.
 st.set_page_config(page_title="WebsiteName", layout="wide")
-st.markdown(
-    """
-    <style>
-    [data-testid="stSidebar"][aria-expanded="true"] > div:first-child{
-        width: 400px;
-    }
-    [data-testid="stSidebar"][aria-expanded="false"] > div:first-child{
-        width: 400px;
-        margin-left: -400px;
-    }
-    """,
 
-    unsafe_allow_html=True,
-)
 def addpoint(points): #function to add points to user
     config['credentials']['usernames'][username]['points'] += points
 def removepoint(points): #function to remove points from user
