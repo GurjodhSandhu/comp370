f21b9f62e3caa3246a7578d12cc50b06546942be
GurjodhSandhu <114323787+GurjodhSandhu@users.noreply.github.com>
2024-03-27 19:30:47 -0700

    Update 📘Math.py

--- pages/📘Math.py
+++ pages/📘Math.py
@@ -4,20 +4,7 @@ import yaml
 from yaml.loader import SafeLoader
 #Title of the website and makes site in widemode and sets the page font to Lilita One
 st.set_page_config(page_title="WebsiteName", layout="wide")
-st.markdown(
-    """
-    <style>
-    [data-testid="stSidebar"][aria-expanded="true"] > div:first-child{
-        width: 400px;
-    }
-    [data-testid="stSidebar"][aria-expanded="false"] > div:first-child{
-        width: 400px;
-        margin-left: -400px;
-    }
-    """,
 
-    unsafe_allow_html=True,
-)
 
 st.markdown("""<style>
     figure {
