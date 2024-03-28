6ec0eb933dcecd5ef845c878fca1c9eef6164dde
GurjodhSandhu <114323787+GurjodhSandhu@users.noreply.github.com>
2024-03-27 19:12:40 -0700

    Update Login_page.py
    front-end update

--- Login_page.py
+++ Login_page.py
@@ -4,6 +4,22 @@ import streamlit as st
 import yaml
 from yaml.loader import SafeLoader
 
+# Sets page in widemode,specifically allowing header to span across page.
+st.set_page_config(page_title="WebsiteName", layout="wide")
+st.markdown(
+    """
+    <style>
+    [data-testid="stSidebar"][aria-expanded="true"] > div:first-child{
+        width: 400px;
+    }
+    [data-testid="stSidebar"][aria-expanded="false"] > div:first-child{
+        width: 400px;
+        margin-left: -400px;
+    }
+    """,
+
+    unsafe_allow_html=True,
+)
 def addpoint(points): #function to add points to user
     config['credentials']['usernames'][username]['points'] += points
 def removepoint(points): #function to remove points from user
@@ -64,20 +80,49 @@ if st.session_state["authentication_status"]:
         config['credentials']['usernames'][username]['points'] = 1 #set users point balance to 1
 
 
-
-#--------------------------------page code start ----------------------------------
-
+#----------------------page code start---------------------------------
 if st.session_state["authentication_status"]: #if the user is authenticated currently
     pointbalance = config['credentials']['usernames'][username]['points']  # amount of points a user currently has
     badge = badgeidtoimage(
         config['credentials']['usernames'][username]['selectbadgeid'])  # badge user currently has selected
+    # Create container for header
+    st.markdown(
+        """
+        <style>
+        @import url('https://fonts.googleapis.com/css2?family=Lilita+One&display=swap');
+        .title-container {
+            display: flex;
+            align-items: center;
+            padding: 10px;
+            background-color: #FFE2E0;
+            border-radius: 10px;
+            box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+            font-size: 50px; 
+            margin-top: -105px; 
+            font-family: 'Lilita One', cursive; 
+            color: #FF8C84
 
+        }
+        .image-container {
+            margin-left: 20px; /* Adjust the margin to create space between title and image */
+        }
+        </style>
+        """,
+        unsafe_allow_html=True
+    )
+    # Display title with colored background and image
+    st.markdown(
+        '<div class="title-container">BrainyBytes Lab<div class="image-container"><img src="https://png2.cleanpng.com/sh/b01c461b1083058d46fffedf4c4f8b8a/L0KzQYm3VcA1N5RrfZH0aYP2gLBuTfJzaZpzRdZ7YYfsfri0gBxqeF5miuY2NXHocrXqhvEybZRneqY3NUm7RIa4VsYyPWM6TKIBOUezQYO9Ur5xdpg=/kisspng-brain-drawing-clip-art-5aebdcfa1ecbb4.5984516615254069701262.png" width="100"></div></div>',
+        unsafe_allow_html=True
+    )
+    # Display title with colored background and image
+    st.markdown(
+        '<div class="title-container">BrainyBytes Lab<div class="image-container"><img src="https://png2.cleanpng.com/sh/b01c461b1083058d46fffedf4c4f8b8a/L0KzQYm3VcA1N5RrfZH0aYP2gLBuTfJzaZpzRdZ7YYfsfri0gBxqeF5miuY2NXHocrXqhvEybZRneqY3NUm7RIa4VsYyPWM6TKIBOUezQYO9Ur5xdpg=/kisspng-brain-drawing-clip-art-5aebdcfa1ecbb4.5984516615254069701262.png" width="100"></div></div>',
+        unsafe_allow_html=True)
     #do stuff here
     challenges = pd.read_csv('challenges.csv')
     #main page code
-    st.title("HOME PAGE")
     st.write(f'Welcome *{st.session_state["name"]}*')
-
     st.write(f'Challenge #1: :red[*{challenges["Challenge"][0]}*] worth :green[ *{challenges["pointsworth"][0]}*] points status: :red[*{challenges["tracker"][0]}*]/:red[*{challenges["tracker2"][0]}*]')
     st.write(f'Challenge #2: :red[*{challenges["Challenge"][1]}*] worth :green[ *{challenges["pointsworth"][1]}*] points status: :red[*{challenges["tracker"][1]}*]/:red[*{challenges["tracker2"][1]}*]')
     st.write(f'Challenge #3: :red[*{challenges["Challenge"][2]}*] worth :green[ *{challenges["pointsworth"][2]}*] points status: :red[*{challenges["tracker"][2]}*]/:red[*{challenges["tracker2"][2]}*]')
@@ -86,11 +131,7 @@ if st.session_state["authentication_status"]: #if the user is authenticated curr
     st.sidebar.write(badge + username)
     st.sidebar.write('you currently have: ' + str(pointbalance) + ' points')
 
-
-
-#--------------------------------page code end ----------------------------------
-
-
+#----------------------page code end---------------------------------
 
 else: #registration for the website
     try:
