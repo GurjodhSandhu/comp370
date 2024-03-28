971df37874c1050ed47dbfdaf95f348b2ad6a92d
GurjodhSandhu <114323787+GurjodhSandhu@users.noreply.github.com>
2024-03-27 19:30:16 -0700

    Update 👨‍💻Coding.py

--- pages/👨‍💻Coding.py
+++ pages/👨‍💻Coding.py
@@ -5,20 +5,6 @@ from yaml.loader import SafeLoader
 
 # Title of the website and makes site in widemode and sets the page font to Lilita One
 st.set_page_config(page_title="WebsiteName", layout="wide")
-st.markdown(
-    """
-    <style>
-    [data-testid="stSidebar"][aria-expanded="true"] > div:first-child{
-        width: 400px;
-    }
-    [data-testid="stSidebar"][aria-expanded="false"] > div:first-child{
-        width: 400px;
-        margin-left: -400px;
-    }
-    """,
-
-    unsafe_allow_html=True,
-)
 
 st.markdown("""<style>
     figure {
