ff14b0db1a2051e92a5fa856042146d1788954d1
GurjodhSandhu <114323787+GurjodhSandhu@users.noreply.github.com>
2024-04-01 20:44:46 -0700

    Update 👤Profile.py

--- pages/👤Profile.py
+++ pages/👤Profile.py
@@ -38,6 +38,7 @@ if st.session_state["authentication_status"]:
         config['credentials']['usernames'][username]['points'] = 1 #set users point balance to 1
 
 if st.session_state["authentication_status"]: #if the user is authenticated currently
+    st.title("Profile Page")
    #function to take a badges image and turn it into the badges id #
     def badgeimagetoid(image):
         if image == "👨":
@@ -105,6 +106,21 @@ if st.session_state["authentication_status"]: #if the user is authenticated curr
 
     config['credentials']['usernames'][username]['selectbadgeid'] = badgeimagetoid(option)
 
+
+if st.session_state["authentication_status"]: #updating user info
+    try:
+        if authenticator.update_user_details(st.session_state["username"]):
+            st.success('Entries updated successfully')
+    except Exception as e:
+        st.error(e)
+
+if st.session_state["authentication_status"]: #reseting password
+    try:
+        if authenticator.reset_password(st.session_state["username"]):
+            st.success('Password modified successfully')
+    except Exception as e:
+        st.error(e)
+
 else: #registration for the website
     try:
         email_of_registered_user, username_of_registered_user, name_of_registered_user = authenticator.register_user(preauthorization=False)
@@ -121,4 +137,3 @@ else: #registration for the website
 
 with open('config.yaml', 'w') as file:
     yaml.dump(config, file, default_flow_style=False)
-
