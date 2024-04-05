cb8efe2b28ee37e259a2ebedece769e3c1ad44e6
GurjodhSandhu <114323787+GurjodhSandhu@users.noreply.github.com>
2024-04-04 17:29:39 -0700

    Update 👨‍💻Coding.py

--- pages/👨‍💻Coding.py
+++ pages/👨‍💻Coding.py
@@ -6,6 +6,21 @@ import json
 import streamlit.components.v1 as components
 # Title of the website and makes site in widemode and sets the page font to Lilita One
 st.set_page_config(page_title="BrainyBytes Lab", layout="wide")
+page_bg_img = f"""
+<style>
+    [data-testid="stAppViewContainer"] > .main {{
+    background-image: url("https://i.postimg.cc/rw2qQm9T/v935-aum-16.jpg");
+    background-size: cover;
+    background-position: center center;
+    background-repeat: no-repeat;
+    background-attachment: local];
+}}
+[data-testid="stHeader"] {{
+    background: rgba(0,0,0,0);
+}}
+</style>
+"""
+
 def multichoicegame(unit,questionfile):
     gamequestion = open(questionfile) #load question file for game
     questions = json.load(gamequestion)
@@ -191,6 +206,60 @@ if st.session_state["authentication_status"]: #if the user is authenticated curr
     with colm2:
         st.image("images/codinginterface.jpg", caption="Coding Interface", width = 300) # display thumbnail for coding interface
         with st.popover("play"):
+            # Set background for page
+            page_bg_img = f"""
+            <style>
+                [data-testid="stAppViewContainer"] > .main {{
+                background-image: url("https://i.postimg.cc/Dzk9yB0c/3175102.jpg");
+                background-size: cover;
+                background-position: center center;
+                background-repeat: no-repeat;
+                background-attachment: local];
+            }}
+            [data-testid="stHeader"] {{
+                background: rgba(0,0,0,0);
+            }}
+            </style>
+            """
+            st.markdown(page_bg_img, unsafe_allow_html=True)
+              # Create container for header
+            st.markdown(
+                     """
+                <style>
+                 @import url('https://fonts.googleapis.com/css2?family=Lilita+One&display=swap');
+                 .title-container {
+                    display: flex;
+                     align-items: center;
+                    padding: 10px;
+                    background-color: #fcc4d4;
+                    border-radius: 10px;
+                    box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+                    font-size: 50px; 
+                    margin-top: -105px; 
+                    font-family: 'Lilita One', cursive; 
+                    color: #d33f73;
+                }
+            .image-container {
+                     margin-left: 20px;
+                    }
+                     div.stButton > Button:first-child {
+                     background-color: #FFE2E0;
+                     color: black;
+                    font-size: 20px;
+                    height: 2em;
+                    width: 12em;
+                    border-radius: 10px 10px 10px 10px;
+                    }
+                    </style>
+                    """,
+                        unsafe_allow_html = True
+                                                )
+            st.markdown(
+                    '<div class="title-container">BrainyBytes Lab<div class="image-container"><img src="https://static.vecteezy.com/system/resources/previews/023/092/211/non_2x/cartoon-cute-smart-human-brain-character-waving-vector.jpg" width="100"></div></div>',
+                    unsafe_allow_html = True
+                                             )
+              # Coding Interface
+            st.markdown(page_bg_img, unsafe_allow_html=True)
             def run_code(code):
                 # function to execute the provided code
                 if code.strip():  # Check if code is not empty
